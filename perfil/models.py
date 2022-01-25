70ad4dd735a8b72c5d453aea53e504eaea2158e3
IsmaelBortoluzzi <ismael.bortoluzzi77@gmail.com>
2022-01-25 15:36:35 -0300

    Criando os models de perfil e pedido. Criada validacao do campo CPF mo model perfil

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,70 @@
+from django.contrib.auth.models import User
 from django.db import models
+from django.forms import ValidationError
+import re
+from utils.validador import valida_cpf
 
-# Create your models here.
+
+class Perfil(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='Usuário')
+    data_nascimento = models.DateField(verbose_name='Data de Nascimento')
+    cpf = models.CharField(max_length=11, verbose_name='CPF')
+    endereco = models.CharField(max_length=50)
+    numero = models.CharField(max_length=5, verbose_name='Número')
+    complemento = models.CharField(max_length=30)
+    bairro = models.CharField(max_length=30)
+    cep = models.CharField(max_length=8)
+    cidade = models.CharField(max_length=30)
+    estado = models.CharField(
+        max_length=2,
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins'),
+        )
+    )
+
+    def __str__(self):
+        return f'{self.usuario}'
+
+    # local das validações
+    def clean(self):
+        error_messages = {}
+
+        if not valida_cpf(self.cpf):
+            error_messages['cpf'] = 'Digite um CPF válido'
+
+        if error_messages:
+            raise ValidationError(error_messages)
+
+
+
+
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfis'
+
