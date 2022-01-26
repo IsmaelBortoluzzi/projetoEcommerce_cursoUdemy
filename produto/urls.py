17ad6b08b68213ea6100c67018da29068b813983
IsmaelBortoluzzi <ismael.bortoluzzi77@gmail.com>
2022-01-26 14:29:06 -0300

    Adicionando os detalhes do produto

--- produto/urls.py
+++ produto/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('', views.ListaProdutos.as_view(), name='lista'),
     path('<slug>', views.DetalheProduto.as_view(), name='detalhe'),
     path('addcarrinho/', views.AddCarrinhoProdutos.as_view(), name='add'),
-    path('removecarrinho', views.RemoveCarrinhoProdutos.as_view(), name='remove'),
+    path('removecarrinho/', views.RemoveCarrinhoProdutos.as_view(), name='remove'),
     path('carrinho/', views.CarrinhoProduto.as_view(), name='carrinho'),
     path('finalizar/', views.FinalizarProduto.as_view(), name='finalizar')
 ]
